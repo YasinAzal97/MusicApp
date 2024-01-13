156945fc5fb681a555c0fc01f5f7f8c156e0c1e4
Yasin <yasinazal@outlook.de>
2024-01-14 00:58:35 +0100

    Location Emb

--- app/src/main/java/com/example/myapplication/db/repo/UserRepository.kt
+++ app/src/main/java/com/example/myapplication/db/repo/UserRepository.kt
@@ -5,14 +5,5 @@ import com.example.myapplication.db.dao.UserDao
 import com.example.myapplication.db.entity.Benutzer
 
 class UserRepository(private val userDao: UserDao) {
-
     val readAllData: LiveData<List<Benutzer>> = userDao.readAllData()
-
-    suspend fun addUser(user: Benutzer) {
-        userDao.insertBenutzer(user)
-    }
-
-    suspend fun searchUser(username: String, password: String) {
-        userDao.getUserByUsernameAndPassword(username,password)
-    }
 }
--- app/src/main/java/com/example/myapplication/db/viewModel/UserViewModel.kt
+++ app/src/main/java/com/example/myapplication/db/viewModel/UserViewModel.kt
@@ -3,47 +3,27 @@ package com.example.myapplication.db.viewModel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.viewModelScope
 import com.example.myapplication.db.UserDatabase
 import com.example.myapplication.db.entity.Benutzer
 import com.example.myapplication.db.repo.UserRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 
 class UserViewModel(application: Application): AndroidViewModel(application) {
 
     // LiveData für die Liste aller Benutzer
-    private val readAllData: LiveData<List<Benutzer>>
     // Repository zur Behandlung von Datenzugriffe
+    private val readAllData: LiveData<List<Benutzer>>
     private val repository: UserRepository
 
     public var db: UserDatabase = UserDatabase.getDatabase(application);
     init {
         // Zugriff auf die UserDao-Datenbank
-        val userDao = db.userDao()
         // Initialisierung des UserRepository mit dem UserDao
-        repository = UserRepository(userDao)
         // Abrufen der LiveData-Liste aller Benutzer über das Repository
+        val userDao = db.userDao()
+        repository = UserRepository(userDao)
         readAllData = repository.readAllData
     }
 
-    // Funktion, um einen Benutzer hinzuzufügen
-    fun addUser(user: Benutzer){
-        // Startet eine Coroutine im Hintergrund für die Datenbankoperation
-        viewModelScope.launch(Dispatchers.IO) {
-            // Fügt den Benutzer über das Repository hinzu
-            repository.addUser(user)
-        }
-    }
-
-    // Funktion, um einen Benutzer hinzuzufügen
-    fun searchUser(username: String, password: String){
-        // Startet eine Coroutine im Hintergrund für die Datenbankoperation
-        viewModelScope.launch(Dispatchers.IO) {
-            // Fügt den Benutzer über das Repository hinzu
-            repository.searchUser(username, password)
-        }
-    }
 
 }
