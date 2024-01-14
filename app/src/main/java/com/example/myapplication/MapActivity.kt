2453f5a650f1b704ebe925834c22d77db890f1ec
Yasin <yasinazal@outlook.de>
2024-01-14 04:09:36 +0100

    GoogleMaps mit Radius Function

--- app/src/main/java/com/example/myapplication/MapActivity.kt
+++ app/src/main/java/com/example/myapplication/MapActivity.kt
@@ -1,22 +1,23 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 
-internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -25,13 +26,26 @@ internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions()
-            .position(sydney)
-            .title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Setze den Click-Listener für die Karte
+        mMap.setOnMapClickListener(this)
+    }
+
+    override fun onMapClick(latLng: LatLng) {
+        // Entferne vorherige Kreise (optional)
+        mMap.clear()
+
+        // Füge einen Kreis mit einem Radius von 100 km an der angeklickten Position hinzu
+        val circleOptions = CircleOptions()
+            .center(latLng)
+            .radius(200000.0) // Radius in Metern (100 km = 100,000 Meter)
+            .strokeColor(Color.YELLOW) // Farbe des Kreisrands
+            .fillColor(Color.argb(70, 0, 0, 255)) // Füllfarbe des Kreises mit Transparenz
+        mMap.addCircle(circleOptions)
+
+        // Optional: Bewege die Kamera zur Klick-Position
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
     }
 }
+
 
       
